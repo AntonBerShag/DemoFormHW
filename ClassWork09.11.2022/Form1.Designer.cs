05edfbbe65af4b6822d940236d6b9078a63ff30c
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-11-14 01:08:38 +0500

    Add project files.

---
+++ ClassWork09.11.2022/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ClassWork09._11._2022
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelResoltion2 = new System.Windows.Forms.Label();
+            this.buttonMake8angle = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.richTextBoxRun = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelResoltion2
+            // 
+            this.labelResoltion2.AutoSize = true;
+            this.labelResoltion2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResoltion2.Location = new System.Drawing.Point(48, 38);
+            this.labelResoltion2.Name = "labelResoltion2";
+            this.labelResoltion2.Size = new System.Drawing.Size(205, 24);
+            this.labelResoltion2.TabIndex = 1;
+            this.labelResoltion2.Text = "Разрешение экрана";
+            // 
+            // buttonMake8angle
+            // 
+            this.buttonMake8angle.Location = new System.Drawing.Point(105, 12);
+            this.buttonMake8angle.Name = "buttonMake8angle";
+            this.buttonMake8angle.Size = new System.Drawing.Size(75, 23);
+            this.buttonMake8angle.TabIndex = 2;
+            this.buttonMake8angle.Text = "8-угольник";
+            this.buttonMake8angle.UseVisualStyleBackColor = true;
+            this.buttonMake8angle.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonMake8angle_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(67, 207);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(103, 23);
+            this.buttonRun.TabIndex = 4;
+            this.buttonRun.Text = "Выбрать файл";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonRun_MouseClick);
+            // 
+            // richTextBoxRun
+            // 
+            this.richTextBoxRun.Location = new System.Drawing.Point(52, 155);
+            this.richTextBoxRun.Name = "richTextBoxRun";
+            this.richTextBoxRun.Size = new System.Drawing.Size(309, 46);
+            this.richTextBoxRun.TabIndex = 5;
+            this.richTextBoxRun.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxRun);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.buttonMake8angle);
+            this.Controls.Add(this.labelResoltion2);
+            this.Name = "Form1";
+            this.Text = "Расширение экрана";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick_1);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelResoltion2;
+        private System.Windows.Forms.Button buttonMake8angle;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.RichTextBox richTextBoxRun;
+    }
+}
+
---
+++ ClassWork09.11.2022/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassWork09._11._2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void myFormSize()
+        {
+            var actualSize = SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            actualSize.Width /= 4;
+            actualSize.Height /= 4;
+            this.MaximumSize = this.MinimumSize = actualSize;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //var mSize = SystemInformation.PrimaryMonitorSize;
+            var actualSize = SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            /*int width = mSize.Width;
+            int height = mSize.Height;*/
+            labelResoltion2.Text = /*$"Ширина = {width}, высота = {height}" +*/
+                $"\nАктивная область:" +
+                $"\nШирина = {actualSize.Width}, высота = {actualSize.Height}";
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            myFormSize();
+            labelResoltion2.Text = $"\nРазмер окна:" +
+                $"\nШирина = {this.Width}, высота = {this.Height}";
+        }
+
+        private void Form1_DoubleClick_1(object sender, EventArgs e)
+        {
+           /* var fileContent = string.Empty;
+            var filePath = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "c_sharp files (*.cs)|*.cs|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                var fileStream = openFileDialog.OpenFile();
+
+                //using (StreamReader reader = new StreamReader(fileStream))
+                //{
+                //    fileContent = reader.ReadToEnd();
+                //}
+                StreamReader reader = new StreamReader(fileStream);
+                fileContent = reader.ReadToEnd();
+                reader.Close();
+            }
+            MessageBox.Show(fileContent, "File Content at path: " + filePath, MessageBoxButtons.OK);*/
+        }
+
+        private void transform()
+        {
+            var height = this.Height;
+            var width = this.Width;
+            System.Drawing.Drawing2D.GraphicsPath myPath = new System.Drawing.Drawing2D.GraphicsPath();
+            myPath.AddPolygon(new Point[] { new Point(width/3, 0), //Верхняя левая точка
+                                            new Point((width/3)*2, 0), //Верхняя правая точка
+                                            new Point(width, height/3),
+                                            new Point(width, (height/3)*2),
+                                            new Point((width/3)*2, height), //Нижняя правая точка
+                                            new Point(width/3, height), //Нижняя левая точка
+                                            new Point(0, (height/3)*2),
+                                            new Point(0, height/3)
+            });
+            Region myRegiion = new Region(myPath);
+            this.Region = myRegiion;
+        }
+
+        private void buttonMake8angle_MouseClick(object sender, MouseEventArgs e)
+        {
+            transform();
+        }
+
+        //бегущая строка
+        string Content { set; get; }
+        private string openFile()
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "c_sharp files (*.cs)|*.cs|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                var fileStream = openFileDialog.OpenFile();
+                StreamReader reader = new StreamReader(fileStream);
+                fileContent = reader.ReadToEnd();
+                reader.Close();
+            }
+            return filePath;
+        }
+        private void ticker(string file)
+        {
+            var sr = new StreamReader(file);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                richTextBoxRun.Text += line;
+            }
+            sr.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ticker(this.Content);
+        }
+
+        private void choiseButton()
+        {
+            if (buttonRun.Text == "Выбрать файл")
+            {
+                this.Content = openFile();
+                timer1.Enabled = true;
+                buttonRun.Text = "Стоп";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                buttonRun.Text = "Выбрать файл";
+            }
+        }
+        private void buttonRun_MouseClick(object sender, MouseEventArgs e)
+        {
+            choiseButton();
+        }
+    }
+}
